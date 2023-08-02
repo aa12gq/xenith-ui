2fbc6b9ac551b7cdd01b2707f2bb51c917ccf315
aa12 <875151567@qq.com>
2023-08-02 21:54:28 +0800

    chore: 移除 vite.config.ts文件，增加示例文件

--- vite.config.example.ts
+++ vite.config.example.ts
@@ -12,5 +12,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+        '/api': {
+            target: 'http://url:port',
+            changeOrigin: true,
+            rewrite: path => path.replace(/^\/api/, ''),
+        },
+    },
+},
 })
